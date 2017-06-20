22dd41a32db6ff53433766ce34e0a45e818a6c5a
ASUSW\DGabriele <dgabriele3780@gmail.com>
2017-06-20 16:15:02 -0400

    Datos de Clientes

--- core/controllers/clientesController.php
+++ core/controllers/clientesController.php
@@ -1,5 +1,41 @@
 <?php
+$db = new Conexion();
+$sql = $db->query("SELECT * FROM clientes;");
+if($db->rows($sql) > 0) {
+  // crear la tabla de clientes
+  $tbClientes='
+  <table id="tbclientes" class="table table-striped table-bordered">
+  <thead>
+  <tr>
+  <th>RIF</th>
+  <th>NOMBRE</th>
+  <th>CORREO</th>
+  <th>Acciones</th>
+  </tr>
+  </thead><tbody>';
+  while($data=$db->recorrer($sql)){
+    $tbClientes.='  <tr>
+    <td>'.$data['rif'].'</td>
+    <td>'.$data['nombres'].'</td>
+    <td>'.$data['correo'].'</td>
+    <td>
+    <button class="btn btn-success btn-rounded btn-sm" data-toggle="modal" data-target=".detallesC" title="Detalles" onclick="detallesC('.$data['id'].')"><i class="glyphicon glyphicon-search"></i></button>
+    <button class="btn btn-warning btn-rounded btn-sm" title="Editar" onclick="editarC('.$data['id'].')"><i class="glyphicon glyphicon-pencil"></i></button>
+    </td>
+    </tr>';
+  }
+  $tbClientes.='</tbody></table>';
+} else {
+  // aun no hay clientes registrados
+  $tbClientes=
+  '<div class="alert alert-warning">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  Aún no hay registros de clientes.
+  </div>';
+}
+$db->liberar($sql);
+$db->close();
 
 include('html/clientes/clientes.php');
 
- ?>
+?>
--- views/js/clientes.js
+++ views/js/clientes.js
@@ -1,120 +1,71 @@
 
-    jQuery(document).ready(function(){
+jQuery(document).ready(function(){
+  jQuery('#tbclientes').DataTable({
+    language:  {
+      "sProcessing":     "Procesando...",
+      "sLengthMenu":     "Mostrar _MENU_ registros",
+      "sZeroRecords":    "No se encontraron resultados",
+      "sEmptyTable":     "Ningún dato disponible en esta tabla",
+      "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+      "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+      "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+      "sInfoPostFix":    "",
+      "sSearch":         "Buscar:",
+      "sUrl":            "",
+      "sInfoThousands":  ",",
+      "sLoadingRecords": "Cargando...",
+      "oPaginate": {
+        "sFirst":    "Primero",
+        "sLast":     "Último",
+        "sNext":     "Siguiente",
+        "sPrevious": "Anterior"
+      },
+      "oAria": {
+        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+      }
+    },
+    responsive: true
+  });
+});
 
-        jQuery('#basicTable').DataTable({
-          	language:  {
-              "sProcessing":     "Procesando...",
-              "sLengthMenu":     "Mostrar _MENU_ registros",
-              "sZeroRecords":    "No se encontraron resultados",
-              "sEmptyTable":     "Ningún dato disponible en esta tabla",
-              "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-              "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-              "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-              "sInfoPostFix":    "",
-              "sSearch":         "Buscar:",
-              "sUrl":            "",
-              "sInfoThousands":  ",",
-              "sLoadingRecords": "Cargando...",
-              "oPaginate": {
-                  "sFirst":    "Primero",
-                  "sLast":     "Último",
-                  "sNext":     "Siguiente",
-                  "sPrevious": "Anterior"
-              },
-              "oAria": {
-                  "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-                  "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-              }
-          },
-            responsive: true
-        });
+function detallesC(e){
+  var id=e;
+  var Objeto = new Object();
+  Objeto.id = id;
+  var data= {id:Objeto};
+  $.post('ajax.php?mode=datacliente',data,
+  function(data){
+    if(data.valid==true){
+      var respuesta, telefonos;
+      respuesta = JSON.parse(data.respuesta);
+      telefonos = JSON.parse(data.telefonos);
 
-        var shTable = jQuery('#shTable').DataTable({
-            "fnDrawCallback": function(oSettings) {
-                jQuery('#shTable_paginate ul').addClass('pagination-active-dark');
-            },
-            responsive: true
-        });
+      $('#tituloC').html(respuesta[id].nombres);
+      $('#rifC').val(respuesta[id].rif);
+      $('#correoC').val(respuesta[id].correo);
+      $('#direccionC').html(respuesta[id].direccion);
 
-        // Show/Hide Columns Dropdown
-        jQuery('#shCol').click(function(event){
-            event.stopPropagation();
-        });
-
-        jQuery('#shCol input').on('click', function() {
-
-            // Get the column API object
-            var column = shTable.column($(this).val());
-
-            // Toggle the visibility
-            if ($(this).is(':checked'))
-                column.visible(true);
-            else
-                column.visible(false);
-        });
-
-        var exRowTable = jQuery('#exRowTable').DataTable({
-            responsive: true,
-            "fnDrawCallback": function(oSettings) {
-                jQuery('#exRowTable_paginate ul').addClass('pagination-active-success');
-            },
-            "ajax": "ajax/objects.txt",
-            "columns": [
-                {
-                    "class":          'details-control',
-                    "orderable":      false,
-                    "data":           null,
-                    "defaultContent": ''
-                },
-                { "data": "name" },
-                { "data": "position" },
-                { "data": "office" },
-                { "data": "salary" }
-            ],
-            "order": [[1, 'asc']]
-        });
-
-        // Add event listener for opening and closing details
-        jQuery('#exRowTable tbody').on('click', 'td.details-control', function () {
-            var tr = $(this).closest('tr');
-            var row = exRowTable.row( tr );
-
-            if ( row.child.isShown() ) {
-                // This row is already open - close it
-                row.child.hide();
-                tr.removeClass('shown');
-            }
-            else {
-                // Open this row
-                row.child( format(row.data()) ).show();
-                tr.addClass('shown');
-            }
-        });
-
-
-        // DataTables Length to Select2
-        jQuery('div.dataTables_length select').removeClass('form-control input-sm');
-        jQuery('div.dataTables_length select').css({width: '60px'});
-        jQuery('div.dataTables_length select').select2({
-            minimumResultsForSearch: -1
-        });
-
-    });
-
-    function format (d) {
-        // `d` is the original data object for the row
-        return '<table class="table table-bordered nomargin">'+
-            '<tr>'+
-                '<td>Full name:</td>'+
-                '<td>'+d.name+'</td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td>Extension number:</td>'+
-                '<td>'+d.extn+'</td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td>Extra info:</td>'+
-                '<td>And any further details here (images etc)...</td>'+
-            '</tr>'+
-        '</table>';
+      var trs=$("#telefonoC tr").length;
+      while(trs>1){
+        $('#telefonoC tr:last').remove();
+        trs--;
+      }
+      var contenido='';
+      $.each(telefonos, function(i, item) {
+      contenido=contenido+"<tr><td>"+item.tipo+"</td><td>"+item.numero+"<td></tr>";
+      });
+      $('#telefonoC').append(contenido);
     }
+    else
+    {
+      result = '<div class="alert alert-dismissible alert-warning">';
+      result += '<button type="button" class="close" data-dismiss="danger">&times;</button>';
+      result += '<h4><strong>ERROR!</strong></h4>';
+      result += '<p>Ocurrió un error en la consulta.</p>';
+      result += '</div>';
+      __('__AJAX_DETALLES__').innerHTML = result;
+    }
+  },"json");
+
+}
